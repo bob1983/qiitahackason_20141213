d0b110a40b05e859545ce11b91c65c2a16424709
t-kanda <t-kanda@phybbit.com>
2014-12-13 12:15:43 +0900

    Initial Commit

---
+++ qiitawars/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  qiitawars
+//
+//  Created by 神田 on 2014/12/13.
+//  Copyright (c) 2014年 janehouse. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ qiitawars/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  qiitawars
+//
+//  Created by 神田 on 2014/12/13.
+//  Copyright (c) 2014年 janehouse. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
