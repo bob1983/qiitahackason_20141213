b7f3b424b08b2088815f256ae564aeb9a5dffa6d
bob1983 <bob.1983.g@gmail.com>
2014-12-13 15:31:06 +0900

    Added retrieve process

--- qiitawars/JANQiitaUserInfoService.h
+++ qiitawars/JANQiitaUserInfoService.h
@@ -7,7 +7,16 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "JANQiitaUserInfo.h"
+
+typedef void(^QiitaUserInfoServiceRetrieveSuccessHandler)(JANQiitaUserInfo *);
+typedef void(^QiitaUserInfoServiceRetrieveFailedHandler)();
 
 @interface JANQiitaUserInfoService : NSObject
+
+// TODO: 多分公開する必要ない。というか別のクラスでやるべき
+- (void)retrieveQiitaUserInfoWithUserId:(NSString *)userId
+                         successHandler:(QiitaUserInfoServiceRetrieveSuccessHandler)successHandler
+                          failedHandler:(QiitaUserInfoServiceRetrieveFailedHandler)failedHandler;
 
 @end
