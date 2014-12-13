696654f5d486d8ece0c22e207cebb2f0158421e3
bob1983 <bob.1983.g@gmail.com>
2014-12-13 13:19:33 +0900

    Added models

---
+++ qiitawars/JANQiitaUserInfoService.m
@@ -0,0 +1,13 @@
+//
+//  JANQiitaUserInfoService.m
+//  qiitawars
+//
+//  Created by bob on 2014/12/13.
+//  Copyright (c) 2014年 janehouse. All rights reserved.
+//
+
+#import "JANQiitaUserInfoService.h"
+
+@implementation JANQiitaUserInfoService
+
+@end
